b00d39d38b7a5f7dff9d41681684c5a4ffcef1ae
Anneli <anneli.lofstedt1@abdn.ac.uk>
2022-07-14 16:50:20 +0100

    update project path

---
+++ Data_filepath.R
@@ -0,0 +1,8 @@
+##################################################################
+###
+### This script is to store the file path to the data, to avoid having the file path openly displayed on github.
+### The file name of this script needs to be added to the .gitignore-file
+### You use will the source()-function from R to call and execute this in the main script
+
+### Enter File path to folder with the data below
+data_dir <-"L:/Research Projects/RINH/RESAS/RI-B5-04/PrimaryResearchData/csv-files"
